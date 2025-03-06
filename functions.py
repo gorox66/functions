0c5910ba6ca1981831911f7e6db509962e05cbab
gorox66 <siotka@list.ru>
2025-03-06 18:26:20 +0300

    Добавлена функция filther_equal
    она возвращает массив с элементами равными value

--- functions.py
+++ functions.py
@@ -17,3 +17,13 @@ def filther_less(arr, value):
     
         print(f"Массив с элементами меньше value: {new_lst}")
         return new_lst
+
+def filther_equal(arr, value):
+            new_lst = []
+
+            for element in arr:
+                if element == value:
+                    new_lst.append(element)
+    
+            print(f"Массив с элементами, которые равны value: {value}")
+            return new_lst
