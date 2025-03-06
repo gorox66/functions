765dbe236d490e9315724cacbcd0a7cd52dc3b9f
gorox66 <siotka@list.ru>
2025-03-06 18:19:59 +0300

    Добавлена функция filther_less
    она возвращает массив с элементами меньше value

--- functions.py
+++ functions.py
@@ -7,3 +7,13 @@ def filter_greather(arr, value):
     
     print(f"Массив с элементами больше value: {new_lst}")
     return new_lst
+
+def filther_less(arr, value):
+        new_lst = []
+
+        for element in arr:
+            if element < value:
+                new_lst.append(element)
+    
+        print(f"Массив с элементами меньше value: {new_lst}")
+        return new_lst
